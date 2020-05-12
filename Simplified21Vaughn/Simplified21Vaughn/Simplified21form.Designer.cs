814e7550880757590a4aa747b2be1928192272f2
Vaughn Donohue <60893242+Vaughn2003@users.noreply.github.com>
2020-05-12 12:41:08 -0400

    I finished the assignment

---
+++ Simplified21Vaughn/Simplified21Vaughn/Simplified21form.Designer.cs
@@ -0,0 +1,275 @@
+﻿using System;
+
+namespace Simplified21Vaughn
+{
+    partial class frmSimplified21
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblPlayer = new System.Windows.Forms.Label();
+            this.lblDealer = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.mnuFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.mniNewGame = new System.Windows.Forms.ToolStripMenuItem();
+            this.mniExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.lblPlayerCard1 = new System.Windows.Forms.Label();
+            this.lblPlayerCard2 = new System.Windows.Forms.Label();
+            this.lblPlayerCard3 = new System.Windows.Forms.Label();
+            this.lblDealerCard3 = new System.Windows.Forms.Label();
+            this.lblDealerCard2 = new System.Windows.Forms.Label();
+            this.lblDealerCard1 = new System.Windows.Forms.Label();
+            this.btnHit = new System.Windows.Forms.Button();
+            this.btnStay = new System.Windows.Forms.Button();
+            this.lblPlayerTotal = new System.Windows.Forms.Label();
+            this.lblDealerTotal = new System.Windows.Forms.Label();
+            this.btnPlayerTotal = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblPlayer
+            // 
+            this.lblPlayer.AutoSize = true;
+            this.lblPlayer.Font = new System.Drawing.Font("Mongolian Baiti", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPlayer.Location = new System.Drawing.Point(56, 50);
+            this.lblPlayer.Name = "lblPlayer";
+            this.lblPlayer.Size = new System.Drawing.Size(57, 20);
+            this.lblPlayer.TabIndex = 0;
+            this.lblPlayer.Text = "Player";
+            // 
+            // lblDealer
+            // 
+            this.lblDealer.AutoSize = true;
+            this.lblDealer.Font = new System.Drawing.Font("Mongolian Baiti", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDealer.Location = new System.Drawing.Point(56, 239);
+            this.lblDealer.Name = "lblDealer";
+            this.lblDealer.Size = new System.Drawing.Size(65, 23);
+            this.lblDealer.TabIndex = 1;
+            this.lblDealer.Text = "Dealer";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuFile});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(592, 24);
+            this.menuStrip1.TabIndex = 6;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // mnuFile
+            // 
+            this.mnuFile.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mniNewGame,
+            this.mniExit});
+            this.mnuFile.Name = "mnuFile";
+            this.mnuFile.Size = new System.Drawing.Size(37, 20);
+            this.mnuFile.Text = "File";
+            this.mnuFile.Click += new System.EventHandler(this.fileToolStripMenuItem_Click);
+            // 
+            // mniNewGame
+            // 
+            this.mniNewGame.Name = "mniNewGame";
+            this.mniNewGame.Size = new System.Drawing.Size(132, 22);
+            this.mniNewGame.Text = "New Game";
+            this.mniNewGame.Click += new System.EventHandler(this.mniNewGame_Click);
+            // 
+            // mniExit
+            // 
+            this.mniExit.Name = "mniExit";
+            this.mniExit.Size = new System.Drawing.Size(132, 22);
+            this.mniExit.Text = "Exit";
+            this.mniExit.Click += new System.EventHandler(this.mniExit_Click);
+            // 
+            // lblPlayerCard1
+            // 
+            this.lblPlayerCard1.AutoSize = true;
+            this.lblPlayerCard1.Font = new System.Drawing.Font("Mongolian Baiti", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPlayerCard1.Location = new System.Drawing.Point(76, 96);
+            this.lblPlayerCard1.Name = "lblPlayerCard1";
+            this.lblPlayerCard1.Size = new System.Drawing.Size(19, 23);
+            this.lblPlayerCard1.TabIndex = 7;
+            this.lblPlayerCard1.Text = "?";
+            // 
+            // lblPlayerCard2
+            // 
+            this.lblPlayerCard2.AutoSize = true;
+            this.lblPlayerCard2.Font = new System.Drawing.Font("Mongolian Baiti", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPlayerCard2.Location = new System.Drawing.Point(132, 96);
+            this.lblPlayerCard2.Name = "lblPlayerCard2";
+            this.lblPlayerCard2.Size = new System.Drawing.Size(19, 23);
+            this.lblPlayerCard2.TabIndex = 8;
+            this.lblPlayerCard2.Text = "?";
+            // 
+            // lblPlayerCard3
+            // 
+            this.lblPlayerCard3.AutoSize = true;
+            this.lblPlayerCard3.Font = new System.Drawing.Font("Mongolian Baiti", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPlayerCard3.Location = new System.Drawing.Point(187, 96);
+            this.lblPlayerCard3.Name = "lblPlayerCard3";
+            this.lblPlayerCard3.Size = new System.Drawing.Size(19, 23);
+            this.lblPlayerCard3.TabIndex = 9;
+            this.lblPlayerCard3.Text = "?";
+            // 
+            // lblDealerCard3
+            // 
+            this.lblDealerCard3.AutoSize = true;
+            this.lblDealerCard3.Font = new System.Drawing.Font("Mongolian Baiti", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDealerCard3.Location = new System.Drawing.Point(187, 300);
+            this.lblDealerCard3.Name = "lblDealerCard3";
+            this.lblDealerCard3.Size = new System.Drawing.Size(19, 23);
+            this.lblDealerCard3.TabIndex = 12;
+            this.lblDealerCard3.Text = "?";
+            // 
+            // lblDealerCard2
+            // 
+            this.lblDealerCard2.AutoSize = true;
+            this.lblDealerCard2.Font = new System.Drawing.Font("Mongolian Baiti", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDealerCard2.Location = new System.Drawing.Point(132, 300);
+            this.lblDealerCard2.Name = "lblDealerCard2";
+            this.lblDealerCard2.Size = new System.Drawing.Size(19, 23);
+            this.lblDealerCard2.TabIndex = 11;
+            this.lblDealerCard2.Text = "?";
+            // 
+            // lblDealerCard1
+            // 
+            this.lblDealerCard1.AutoSize = true;
+            this.lblDealerCard1.Font = new System.Drawing.Font("Mongolian Baiti", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDealerCard1.Location = new System.Drawing.Point(76, 300);
+            this.lblDealerCard1.Name = "lblDealerCard1";
+            this.lblDealerCard1.Size = new System.Drawing.Size(19, 23);
+            this.lblDealerCard1.TabIndex = 10;
+            this.lblDealerCard1.Text = "?";
+            // 
+            // btnHit
+            // 
+            this.btnHit.Font = new System.Drawing.Font("Mongolian Baiti", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnHit.Location = new System.Drawing.Point(302, 96);
+            this.btnHit.Name = "btnHit";
+            this.btnHit.Size = new System.Drawing.Size(91, 30);
+            this.btnHit.TabIndex = 13;
+            this.btnHit.Text = "Hit";
+            this.btnHit.UseVisualStyleBackColor = true;
+            this.btnHit.Click += new System.EventHandler(this.btnHit_Click);
+            // 
+            // btnStay
+            // 
+            this.btnStay.Font = new System.Drawing.Font("Mongolian Baiti", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStay.Location = new System.Drawing.Point(399, 96);
+            this.btnStay.Name = "btnStay";
+            this.btnStay.Size = new System.Drawing.Size(91, 30);
+            this.btnStay.TabIndex = 14;
+            this.btnStay.Text = "Stay";
+            this.btnStay.UseVisualStyleBackColor = true;
+            this.btnStay.Click += new System.EventHandler(this.btnStay_Click);
+            // 
+            // lblPlayerTotal
+            // 
+            this.lblPlayerTotal.AutoSize = true;
+            this.lblPlayerTotal.Font = new System.Drawing.Font("Mongolian Baiti", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPlayerTotal.Location = new System.Drawing.Point(188, 50);
+            this.lblPlayerTotal.Name = "lblPlayerTotal";
+            this.lblPlayerTotal.Size = new System.Drawing.Size(49, 20);
+            this.lblPlayerTotal.TabIndex = 15;
+            this.lblPlayerTotal.Text = "Total";
+            // 
+            // lblDealerTotal
+            // 
+            this.lblDealerTotal.AutoSize = true;
+            this.lblDealerTotal.Font = new System.Drawing.Font("Mongolian Baiti", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDealerTotal.Location = new System.Drawing.Point(188, 242);
+            this.lblDealerTotal.Name = "lblDealerTotal";
+            this.lblDealerTotal.Size = new System.Drawing.Size(49, 20);
+            this.lblDealerTotal.TabIndex = 16;
+            this.lblDealerTotal.Text = "Total";
+            // 
+            // btnPlayerTotal
+            // 
+            this.btnPlayerTotal.Font = new System.Drawing.Font("Mongolian Baiti", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPlayerTotal.Location = new System.Drawing.Point(302, 50);
+            this.btnPlayerTotal.Name = "btnPlayerTotal";
+            this.btnPlayerTotal.Size = new System.Drawing.Size(188, 30);
+            this.btnPlayerTotal.TabIndex = 17;
+            this.btnPlayerTotal.Text = "Calculate Player Total";
+            this.btnPlayerTotal.UseVisualStyleBackColor = true;
+            this.btnPlayerTotal.Click += new System.EventHandler(this.btnPlayerTotal_Click);
+            // 
+            // frmSimplified21
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(592, 388);
+            this.Controls.Add(this.btnPlayerTotal);
+            this.Controls.Add(this.lblDealerTotal);
+            this.Controls.Add(this.lblPlayerTotal);
+            this.Controls.Add(this.btnStay);
+            this.Controls.Add(this.btnHit);
+            this.Controls.Add(this.lblDealerCard3);
+            this.Controls.Add(this.lblDealerCard2);
+            this.Controls.Add(this.lblDealerCard1);
+            this.Controls.Add(this.lblPlayerCard3);
+            this.Controls.Add(this.lblPlayerCard2);
+            this.Controls.Add(this.lblPlayerCard1);
+            this.Controls.Add(this.lblDealer);
+            this.Controls.Add(this.lblPlayer);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "frmSimplified21";
+            this.Text = "Simplified 21 by Vaughn";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private void mniExit_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblPlayer;
+        private System.Windows.Forms.Label lblDealer;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem mnuFile;
+        private System.Windows.Forms.ToolStripMenuItem mniNewGame;
+        private System.Windows.Forms.ToolStripMenuItem mniExit;
+        private System.Windows.Forms.Label lblPlayerCard1;
+        private System.Windows.Forms.Label lblPlayerCard2;
+        private System.Windows.Forms.Label lblPlayerCard3;
+        private System.Windows.Forms.Label lblDealerCard3;
+        private System.Windows.Forms.Label lblDealerCard2;
+        private System.Windows.Forms.Label lblDealerCard1;
+        private System.Windows.Forms.Button btnHit;
+        private System.Windows.Forms.Button btnStay;
+        private System.Windows.Forms.Label lblPlayerTotal;
+        private System.Windows.Forms.Label lblDealerTotal;
+        private System.Windows.Forms.Button btnPlayerTotal;
+    }
+}
+
---
+++ Simplified21Vaughn/Simplified21Vaughn/Simplified21form.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Simplified21Vaughn
+{
+    public partial class frmSimplified21 : Form
+    {
+        // Declare global constants
+        const int MIN = 1;
+        const int MAX = 11;
+        //Declare global variables
+        Random randomNumGenerator = new Random();
+        int DealerCard1 = 0;
+        int DealerCard2 = 0;
+        int DealerCard3 = 0;
+        int DealerTotal = 0;
+        
+        int PlayerCard1 = 0;
+        int PlayerCard2 = 0;
+        int PlayerCard3 = 0;
+        int PlayerTotal = 0;
+        public frmSimplified21()
+        {
+            InitializeComponent();
+            this.btnHit.Enabled = false;
+            this.btnStay.Enabled = false;
+            this.btnPlayerTotal.Enabled = false;
+        
+        }
+
+        private void fileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void mniNewGame_Click(object sender, EventArgs e)
+        {
+            this.btnHit.Enabled = true;
+            this.btnStay.Enabled = true;
+            this.btnPlayerTotal.Enabled = true;
+
+            //Give dealer 3 random cards between 1-10
+            DealerCard1 = randomNumGenerator.Next(MIN, MAX + 1);
+            DealerCard2 = randomNumGenerator.Next(MIN, MAX + 1);
+            DealerCard3 = randomNumGenerator.Next(MIN, MAX + 1);
+
+            //Give the player 2 random cards between 1-10
+            PlayerCard1 = randomNumGenerator.Next(MIN, MAX + 1);
+            PlayerCard2 = randomNumGenerator.Next(MIN, MAX + 1);
+            this.lblPlayerCard1.Text = Convert.ToString(PlayerCard1);
+            this.lblPlayerCard2.Text = Convert.ToString(PlayerCard2);
+           
+            //Hide the player's 3rd card, depending on if they choose to hit or stay
+            this.lblPlayerCard3.Hide();
+
+            //change the player's total and display 
+            PlayerTotal = PlayerCard1 + PlayerCard2;
+            lblPlayerTotal.Text = Convert.ToString(PlayerTotal);
+
+        }
+
+        private void btnHit_Click(object sender, EventArgs e)
+        {
+            //player get's 3rd card
+            PlayerCard3 = randomNumGenerator.Next(MIN, MAX + 1);
+            this.lblPlayerCard3.Text = Convert.ToString(PlayerCard3);
+           
+            //display 3rd card
+            this.lblPlayerCard3.Show();
+        }
+
+        private void btnStay_Click(object sender, EventArgs e)
+        {
+            //display dealer's cards
+            this.lblDealerCard1.Text = Convert.ToString(DealerCard1);
+            this.lblDealerCard2.Text = Convert.ToString(DealerCard2);
+            this.lblDealerCard3.Text = Convert.ToString(DealerCard3);
+
+            //calculate the dealers total 
+            DealerTotal = DealerCard1 + DealerCard2 + DealerCard3;
+            lblDealerTotal.Text = Convert.ToString(DealerTotal);
+           
+        }
+
+        private void btnPlayerTotal_Click(object sender, EventArgs e)
+        {
+            PlayerTotal = PlayerCard1 + PlayerCard2 + PlayerCard3;
+            lblPlayerTotal.Text = Convert.ToString(PlayerTotal);
+            this.btnHit.Enabled = false;
+        }
+    }
+    }
+
